a9575f699dde85677ff93f41b016d8e89b68d351
yyy2015 <2456700983@qq.com>
2017-05-22 01:20:22 +0800

    Add selector, has many bugs to fix

--- StatDisplay/src/components/charts/customerFlow/customerFlow.js
+++ StatDisplay/src/components/charts/customerFlow/customerFlow.js
@@ -61,11 +61,13 @@ const HourFlowChart = ({data}) => (
       left: 0,
       bottom: 0,
     }}>
-      <XAxis dataKey="name" />
+      <XAxis dataKey="hour" />
       <YAxis />
       <CartesianGrid strokeDasharray="3 3"/>
       <Tooltip />
-      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+      <Area type="monotone" dataKey="jumpRate" stackId="1" stroke="#8884d8" fill="#8884d8" />
+      <Area type="monotone" dataKey="deepVisit" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+      <Area type="monotone" dataKey="inStoreRate" stackId="1" stroke="#ffc658" fill="#ffc658" />
     </AreaChart>
   </Container>
 );
