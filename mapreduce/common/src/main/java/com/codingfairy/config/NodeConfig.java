8f2ff4565c4d12d24151c91e480683504a54ea23
daixinyan <381675152@qq.com>
2017-09-01 16:55:19 +0800

    Dev: change hadoop webhdfs ip address.

--- mapreduce/common/src/main/java/com/codingfairy/config/NodeConfig.java
+++ mapreduce/common/src/main/java/com/codingfairy/config/NodeConfig.java
@@ -9,9 +9,10 @@ public class NodeConfig {
 
     public static final String RECEIVER = "118.89.166.159:8080";
 
-    public static final String NAME_NODE = "115.29.138.254";
+    public static final String NAME_NODE = "118.190.141.234";
 
     public static final Long MAX_WIFI_DATA_INTERVAL = 1000L*60L*10L;
 
     public static final String HDFS_PATH = "webhdfs://"+NAME_NODE+":50070/";
+
 }
